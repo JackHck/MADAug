73d590c512b6e1632bddd254a9bdee6dcbdc9b7a
Hou Chengkai <96556513+JackHck@users.noreply.github.com>
2022-11-30 08:51:08 +0800

    Create projection.py

---
+++ networks/projection.py
@@ -0,0 +1,37 @@
+import torch
+import torch.nn as nn
+from config import OPS_NAMES
+
+class Projection(nn.Module):
+    def __init__(self, in_features, n_layers, n_hidden=128):
+        super(Projection, self).__init__()
+        self.n_layers = n_layers
+        if self.n_layers > 0:
+            layers = [nn.Linear(in_features, n_hidden), nn.ReLU()]
+            for _ in range(self.n_layers-1):
+                layers.append(nn.Linear(n_hidden, n_hidden))
+                layers.append(nn.ReLU())
+            layers.append(nn.Linear(n_hidden, 2*len(OPS_NAMES)))
+        else:
+            layers = [nn.Linear(in_features, 2*len(OPS_NAMES))]
+        self.projection = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.projection(x)
+    
+class Augment_decision(nn.Module):
+    def __init__(self, in_features, n_layers, n_hidden=128):
+        super(Augment_decision, self).__init__()
+        self.n_layers = n_layers
+        if self.n_layers > 0:
+            layers = [nn.Linear(in_features, n_hidden), nn.ReLU()]
+            for _ in range(self.n_layers-1):
+                layers.append(nn.Linear(n_hidden, n_hidden))
+                layers.append(nn.ReLU())
+            layers.append(nn.Linear(n_hidden, 1))
+        else:
+            layers = [nn.Linear(in_features, 1)]
+        self.projection = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return torch.sigmoid(self.projection(x))
